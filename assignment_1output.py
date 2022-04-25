9daf199f28672f0e1908770f8897829be2ddd20d
shuklavishwash <104249552+shuklavishwash@users.noreply.github.com>
2022-04-25 23:01:35 +0530

    Add files via upload

---
+++ assignment_1output.py
@@ -0,0 +1,36 @@
+
+First Question
+Enter first number 4
+Enyter second number 6
+Enter third number 5
+The average of three numbers is :
+5.0
+
+
+Second Question
+enter your gross income 100000
+Enter your number of dependents 5
+Your income tax is :
+17399.0
+
+Third Question
+Enter your SID 21102077
+Enter your name VISHWASH
+Enter your Gender M
+Enter your course name CIVIL
+Enter your CGPA 9
+[' 21102077', ' VISHWASH ', 'M', ' CIVIL', 9.0]
+
+
+Fourth Question
+Enter marks of students 97
+Enter marks of students 97
+Enter marks of students 95
+Enter marks of students 95
+Enter marks of students 95
+[' 95', ' 95', ' 95', ' 97', ' 97']
+
+
+Fifth Question
+Part a question :  ['Red', 'Green', 'White', 'Pink', 'Yellow']
+Part b of question : ['Red', 'Green', 'White', 'Purple', 'Yellow']
