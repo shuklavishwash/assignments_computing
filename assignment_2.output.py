69850aa5c4ad297442af1257d73f27d7488f51fd
shuklavishwash <104249552+shuklavishwash@users.noreply.github.com>
2022-05-15 23:16:14 +0530

    Add files via upload

---
+++ assignment_2.output.py
@@ -0,0 +1,41 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: C:\Users\Administrator\Desktop\assignment_2.py ==========
+<A>THE LENGTH OF STRING IS 35
+<B>THE REVERSED STRING IS  egaugnal evitisnes esac a si nohtyP
+<E>THE INDEX OF SUBSTRING a is  10
+<F>THE ORIGINAL STRING AFTER REMOVING WHITESPACES IS Pythonisacasesensitivelanguage
+
+
+ENTER YOUR NAME Vishwash shuklA
+ENTER YOUR SID 21102077
+ENTER YOUR DEPARTMENT Civil
+ENTER YOUR CGPA 9.9
+Hey  Vishwash shuklA, Here!
+My SID is 21102077
+I am from  Civil and my CGPA is 9.900000
+
+
+a.  8
+b.  58
+c.  50
+Left shift both a and b with 2 bits respectively are : 224 40
+Right shift a with 2 bits and b with 4 bits respectively are :  14 0
+
+
+Enter first number: 8
+Enter second number: 58
+Enter third number: 50
+THE LARGEST NUMBER IS  58.0
+
+
+ENTER A STRING pronamed
+Yes
+
+
+ENTER FIRST SIDE OF TRIANGLE 3
+ENTER SECOND SIDE OF TRIANGLE 4
+ENTER THIRD SIDE OF TRIANGLE 5
+Yes
+>>> 
