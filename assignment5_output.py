521d9af1ae15efe1fb1630ee263d7bc9a38bbc62
shuklavishwash <104249552+shuklavishwash@users.noreply.github.com>
2022-06-18 20:39:38 +0530

    Create assignment5_output.py

---
+++ assignment5_output.py
@@ -0,0 +1,99 @@
+#question-1
+Enter the the string : My name is Vishwash Shukla.
+.alkuhS hsawhsiV si eman yM 
+
+#question-2
+Enter the minimum value :10
+Enter the maximum value: 50
+Enter the number for divisibility check :8
+16
+24
+32
+40
+48
+
+#question-3
+Enter the length of side:15
+Enter the length of side:11
+Enter the length of side:12
+65.23802572120036
+
+#question-4
+Enter the no. of rows :9
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+#question-5
+Enter the number of rows 8
+
+A
+BC
+DEF
+GHIJ
+KLMNO
+PQRSTU
+VWXYZAB
+CDEFGHIJ
+
+
+#question-6
+Enter the Lowest Range Value : 10
+Enter the Upper Range Value : 100
+The Prime Numbers in this range are : 
+11
+13
+17
+19
+23
+29
+31
+37
+41
+43
+47
+53
+59
+61
+67
+71
+73
+79
+83
+89
+97
+
+#question-7
+The numbers divisible by 11 and are multiple of 7 are : [77, 154, 231, 308, 385, 462]
+  
+#question-8
+Enter the number 10
+Enter the number 10
+Enter the number 22
+Enter the number -22
+Enter the number 22
+Enter the number 0
+Enter the number -22
+Enter the number -3
+Enter the number -78
+Enter the number 45
+Positive numbers are:  [10, 10, 22, 22, 0, 45]
+Negative numbers are:  [-22, -22, -3, -78]
+Even numbers are:  [10, 10, 22, -22, 22, 0, -22, -78]
+Odd numbers are:  [-3, 45]
+Number of times each number occurs in the List {10: 2, 22: 2, -22: 2, 0: 1, -3: 1, -78: 1, 45: 1}
+
+
+#question-9
+Number of words: 5
+Enter the word: a
+Enter the word: S
+Enter the word: S
+Enter the word: a
+Enter the word: i
+Number of occurences:  {'a': 2, 'S': 2, 'i': 1}
