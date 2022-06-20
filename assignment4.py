739b19c7ccb5680eaaa0e5e840fc2eec21983036
shuklavishwash <104249552+shuklavishwash@users.noreply.github.com>
2022-06-20 09:38:22 +0530

    Update assignment4.py

--- assignment4.py
+++ assignment4.py
@@ -18,16 +18,24 @@ else:
     print(" No result ")
 
 #Q.2
-year = int(input("Enter a year: "))
+def checkYear(year):
+    if (year % 4) == 0:
+        if (year % 100) == 0:
+            if (year % 400) == 0:
+                return True
+            else:
+                return False
+        else:
+             return True
+    else:
+        return False
+ 
+year =int(input("Write a year to check if it is a leap year : ")
+if(checkYear(year)):
+    print("Leap Year")
+else:
+    print("Not a Leap Year")
 
-if year % 4 == 0 :
-    print("year is a Leap Year")
-elif year % 100 == 0 :
-    print("year is not a Leap Year")
-elif year % 400 == 0 :
-    print(" year is a Leap Year")
-else :
-    print("year is not a Leap Year")
 
 #Q.3
 import random
